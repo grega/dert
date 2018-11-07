b2282d11a8cc9c8f75671a8cae562ffa4e6f5338
Greg Annandale <greg@raspberrypi.org>
2018-11-07 22:47:52 +0000

    Add verification mailer

---
+++ app/mailers/verification_mailer.rb
@@ -0,0 +1,11 @@
+class VerificationMailer < ApplicationMailer
+  default from: ENV.fetch('EMAIL_FROM')
+ 
+  def verification_email
+    @erasure_request = params[:erasure_request]
+    @email_from = ENV.fetch('ORG_NAME')
+    @web_address = ENV.fetch('WEB_ADDRESS')
+    @url = verify_url(:token => @erasure_request.verify_token)
+    mail(to: @erasure_request.email, subject: "[#{ENV.fetch('ORG_NAME')}] Verify Action: Erase Personal Data")
+  end
+end
