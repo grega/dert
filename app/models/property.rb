7a779333b78927c8c9b895ea3c7de15c2d029e58
Greg Annandale <greg@raspberrypi.org>
2018-11-13 23:36:42 +0000

    Ensure destruction of action on property removal

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ApplicationRecord
 	has_and_belongs_to_many :erasure_requests
-  has_many :removal_action
+  has_many :removal_actions, dependent: :destroy
 
 	validates :name,  presence: true, uniqueness: true
 	validates :url,   presence: true, uniqueness: true
