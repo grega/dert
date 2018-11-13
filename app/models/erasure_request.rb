3a4e617c9363fc23f2c146c6be296e4393ea5fe3
Greg Annandale <greg@raspberrypi.org>
2018-11-13 23:36:14 +0000

    Fulfilled and %fulfilled added to request model

--- app/models/erasure_request.rb
+++ app/models/erasure_request.rb
@@ -1,10 +1,20 @@
 class ErasureRequest < ApplicationRecord
 	has_and_belongs_to_many :properties
-  has_many :removal_actions
+  has_many :removal_actions, dependent: :destroy
 
   accepts_nested_attributes_for :removal_actions
   
 	time_for_a_boolean :verified
 	
 	validates :email,  presence: true, format: { with: /\A.+@.+$\Z/ }, uniqueness: true
+
+  def fulfilled?
+    actions_completed = removal_actions.select{|action| action.completed === true}
+    actions_completed.length === removal_actions.length ? true : false
+  end
+
+  def percent_fulfilled
+    actions_completed = removal_actions.select{|action| action.completed === true}
+    return ((actions_completed.length.to_f / removal_actions.length.to_f) * 100).to_i
+  end
 end
