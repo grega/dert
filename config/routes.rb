e218cda72f1f949e5b3d0044d259185485f7b15f
Greg Annandale <greg@raspberrypi.org>
2018-11-14 20:57:20 +0000

    Place controllers behind /admin path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	get 'verify_erasure_request', to: 'verify#erasure_request', as: 'verify_erasure_request'
 
-	resources :erasure_requests
-	resources :properties
+  scope '/admin' do
+    resources :erasure_requests
+    resources :properties
+  end 
 end
