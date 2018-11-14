4763b09c112700ac59b6785b70d9f853ef3f040d
Greg Annandale <greg@raspberrypi.org>
2018-11-14 20:18:44 +0000

    Move web_console config to dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.web_console.whiny_requests = false
+  config.web_console.whitelisted_ips = '172.25.0.0/16'
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
