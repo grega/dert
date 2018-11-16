91e85b37c19a706bc47325c1d5a97f80ed8fe357
Greg Annandale <greg@raspberrypi.org>
2018-11-16 09:33:02 +0000

    Mailgun prod config

--- app/mailers/verification_mailer.rb
+++ app/mailers/verification_mailer.rb
@@ -4,7 +4,7 @@ class VerificationMailer < ApplicationMailer
   def verification_email
     @erasure_request = params[:erasure_request]
     @email_from = ENV.fetch('ORG_NAME')
-    @web_address = ENV.fetch('WEB_ADDRESS')
+    @site_domain = ENV.fetch('SITE_DOMAIN')
     @url = verify_erasure_request_url(:token => @erasure_request.verify_token)
     mail(to: @erasure_request.email, subject: "[#{ENV.fetch('ORG_NAME')}] Verify Action: Erase Personal Data")
   end
