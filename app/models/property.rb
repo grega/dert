69316fc17c0fd18de416a3dcf37314f401ffebd5
Greg Annandale <g.annandale@gmail.com>
2019-10-07 15:06:04 +0100

    Update property.rb

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,6 @@ class Property < ApplicationRecord
 	has_and_belongs_to_many :erasure_requests
   has_many :removal_actions, dependent: :destroy
 
-	validates :name,  presence: true, uniqueness: true
-	validates :url,   presence: true, uniqueness: true
+  validates :name,  presence: true, uniqueness: true
+  validates :url,   presence: true, uniqueness: true
 end
