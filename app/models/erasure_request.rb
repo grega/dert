33947b3167e4fd85a97102d4d0a1903c8fe1ffc5
Greg Annandale <g.annandale@gmail.com>
2019-10-07 15:05:41 +0100

    Update erasure_request.rb

--- app/models/erasure_request.rb
+++ app/models/erasure_request.rb
@@ -1,12 +1,12 @@
 class ErasureRequest < ApplicationRecord
-	has_and_belongs_to_many :properties
+  has_and_belongs_to_many :properties
   has_many :removal_actions, dependent: :destroy
 
   accepts_nested_attributes_for :removal_actions
 	
-	time_for_a_boolean :verified
+  time_for_a_boolean :verified
 	
-	validates :email,  presence: true, format: { with: /\A.+@.+$\Z/ }, uniqueness: true
+  validates :email,  presence: true, format: { with: /\A.+@.+$\Z/ }, uniqueness: true
 
   def fulfilled?
     actions_completed = removal_actions.select{|action| action.completed === true}
